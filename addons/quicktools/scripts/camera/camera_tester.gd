3686eb5bc8b2e9ea48ff9412bcc659c06f7a46ea
IsaelGSF <isaelgsfaria@gmail.com>
2025-10-07 00:45:35 -0300

    Add fade toggle to camera tester

--- addons/quicktools/scripts/camera/camera_tester.gd
+++ addons/quicktools/scripts/camera/camera_tester.gd
@@ -1,4 +1,6 @@
 extends Node
 
+@export var fade_toggle: CheckButton
+
 func _on_start_shake_button_pressed() -> void:
-	QuickCamera2D.main_camera_start_shake()
+	QuickCamera2D.main_camera_start_shake(30, 1.0, fade_toggle.toggle_mode)
