a657d1376645483f8c1435ef9f3995d5a11fb892
IsaelGSF <isaelgsfaria@gmail.com>
2025-10-07 11:54:31 -0300

    Add target select to camera tester

--- addons/quicktools/scripts/camera/camera_tester.gd
+++ addons/quicktools/scripts/camera/camera_tester.gd
@@ -1,8 +1,33 @@
 extends Node
 
+@export_category("Shake")
 @export var fade_toggle: CheckButton
 @export var strength_text: LineEdit
 @export var duration_text: LineEdit
+
+@export_category("Movement")
+@export_subgroup("Follow")
+@export var target_selectable: OptionButton
+var targetable_nodes: Array[Node2D] = []
+
+func _ready():
+	_fetch_2d_nodes()
+
+func _fetch_2d_nodes() -> void:
+	var current_scene = get_tree().current_scene
+	var nodes: Array[Node] = _get_nodes_in_scene(current_scene)
+	for node in nodes:
+		if node is Node2D:
+			targetable_nodes.append(node)
+			target_selectable.add_item(current_scene.get_path_to(node))
+
+func _get_nodes_in_scene(scene:Node) -> Array[Node]:
+	var nodes: Array[Node] = []
+	for child in scene.get_children():
+		nodes.append(child)
+		nodes.append_array(_get_nodes_in_scene(child))
+	
+	return nodes
 
 func _on_start_shake_button_pressed() -> void:
 	var strength: float = 30.0
