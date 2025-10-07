980862a6fb3875ae6e2e937d40964e492bd609be
IsaelGSF <isaelgsfaria@gmail.com>
2025-10-07 00:39:41 -0300

    Fix shake (timer missing)

--- addons/quicktools/scripts/camera/quick_camera_2d.gd
+++ addons/quicktools/scripts/camera/quick_camera_2d.gd
@@ -15,6 +15,7 @@ var _shake_timer: Timer
 var _shaking: bool = false
 var _shake_fade: bool = false
 var _shake_strength: float = 0.0
+var _current_shake_strength: float = 0.0
 var _shake_duration: float
 
 #endregion
@@ -40,13 +41,16 @@ func _shake_ready() -> void:
 	_shake_timer = Timer.new()
 	_shake_timer.autostart = false
 	_shake_timer.timeout.connect(_on_shake_timer_timeout)
+	add_child(_shake_timer)
 
 func start_shake(strength: float = 30.0, duration: float = 1.0, fade: bool = false) -> void:
 	randomize()
 	_shaking = true
 	_shake_strength = strength
+	_current_shake_strength = strength
 	_shake_fade = fade
 	_shake_duration = duration
+	_shake_timer.start(duration)
 
 static func main_camera_start_shake(strength: float = 30.0, duration: float = 1.0, fade: bool = false) -> void:
 	if not main_camera:
@@ -56,7 +60,7 @@ static func main_camera_start_shake(strength: float = 30.0, duration: float = 1.
 func _process_shake() -> void:
 	if not _shaking: return
 	if _shake_fade:
-		_shake_strength = lerpf(_shake_strength, 0.0, 1.0 - (_shake_timer.time_left / _shake_duration))
+		_current_shake_strength = lerpf(_shake_strength, 0.0, 1.0 - (_shake_timer.time_left / _shake_duration))
 	
 	offset = _random_shake_offset()
 
@@ -65,6 +69,6 @@ func _on_shake_timer_timeout() -> void:
 	offset = Vector2.ZERO
 
 func _random_shake_offset() -> Vector2:
-	return Vector2(randf_range(-_shake_strength, _shake_strength), \
-					randf_range(-_shake_strength, _shake_strength))
+	return Vector2(randf_range(-_current_shake_strength, _current_shake_strength), \
+					randf_range(-_current_shake_strength, _current_shake_strength))
 #endregion
